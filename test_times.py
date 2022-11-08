19f9028b8c6281ef9dbd8e2efd6871c39e39425c
Bronte Wan <bronte288@hotmail.com>
2022-11-08 16:15:36 +0000

    Parameters moved to fixtures.yml

--- test_times.py
+++ test_times.py
@@ -1,39 +1,29 @@
 from times import *
 import pytest
+import yaml
 
 
-@pytest.mark.parametrize("large, short, expected", [
-    #Standard Input
-    (
-        time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
-        time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), 
-        [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-        ), 
+with open('fixtures.yml') as yaml_file:
+    fixtures = yaml.safe_load(yaml_file)
 
-    #Two time ranges that do not overlap
-    (
-       time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
-       time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00"),
-       []
-    ), 
-    
-    #Two time ranges that both contain several intervals each
-    (
-        time_range("2010-01-12 10:00:00", "2010-01-12 10:31:00", 2, 60),
-        time_range("2010-01-12 10:15:00", "2010-01-12 10:46:00", 2, 60),
-        [('2010-01-12 10:15:00', '2010-01-12 10:15:00'), ('2010-01-12 10:16:00', '2010-01-12 10:30:00'), ('2010-01-12 10:31:00', '2010-01-12 10:31:00')]
-    ),
-    
-    #Two time ranges that end exactly at the same time when the other starts
-    (
-        time_range("2010-01-12 10:00:00", "2010-01-12 10:31:00"),
-        time_range("2010-01-12 10:31:00", "2010-01-12 10:46:00"),
-        [('2010-01-12 10:31:00', '2010-01-12 10:31:00')]
-    )
-    ])
+params = [(eval(fixture["long"]), eval(fixture["short"]), [eval(range) for range in fixture["expected"]]) for fixture in fixtures.values()]
+
+
+@pytest.mark.parametrize("large, short, expected", params)
 def test_given_input(large, short, expected):
     result = compute_overlap_time(large, short)
     assert result == expected
+
+def test_time_range_backwards():
+    with pytest.raises(ValueError, match=r"End time: .* is before start time: .*\."):
+        time_range("2010-01-12 10:45:00", "2010-01-12 10:31:00")
+
+"""
+For test coverage, run in terminal:
+pytest --cov="." --cov-report html
+python -m http.server -d htmlcov
+"""
+
 
 # def test_given_input():
 #     """Standard input"""
@@ -66,13 +56,3 @@ def test_given_input(large, short, expected):
 #     result = compute_overlap_time(large, short)
 #     expected = [('2010-01-12 10:31:00', '2010-01-12 10:31:00')]
 #     assert result == expected
-
-def test_time_range_backwards():
-    with pytest.raises(ValueError, match=r"End time: .* is before start time: .*\."):
-        time_range("2010-01-12 10:45:00", "2010-01-12 10:31:00")
-
-"""
-For test coverage, run in terminal:
-pytest --cov="." --cov-report html
-python -m http.server -d htmlcov
-"""
